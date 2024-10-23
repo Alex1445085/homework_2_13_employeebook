10bd572f865923afac942fd2f42abdf6b0568d99
Alex1445085 <n1445085@yandex.ru>
2024-10-23 20:34:33 +0300

    three-quarters of try

--- src/main/java/pro/sky/homework213employee/Emploee.java
+++ src/main/java/pro/sky/homework213employee/Emploee.java
@@ -1,6 +1,8 @@
 package pro.sky.homework213employee;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Emploee {
 
@@ -17,16 +19,41 @@ public class Emploee {
     }
 
     @Override
-    public String toString() { return this.name + " " +  this.secondName + ", dep# " + this.departmentNo + ", salary " + salary + " "; }
+    public String toString() {
+        return this.name + " " + this.secondName + ", dep# " + this.departmentNo + ", salary " + salary + " ";
+    }
 
-    public String getName() { return name; }
-    public String getSecondName() { return secondName; }
-    public Integer getSalary() { return salary; }
-    public Integer getDepartmentNo() { return departmentNo; }
-    public void setName(String name) { this.name = name; }
-    public void setSecondName(String secondName) { this.secondName = secondName; }
-    public void setSalary(Integer salary) { this.salary = salary; }
-    public String makeKey(Emploee temp) { return (temp.getName() + temp.getSecondName()).toLowerCase(); }
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public Integer getDepartmentNo() {
+        return departmentNo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String makeKey(Emploee temp) {
+        return (temp.getName() + temp.getSecondName()).toLowerCase();
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -42,5 +69,20 @@ public class Emploee {
     @Override
     public int hashCode() {
         return Objects.hash(name, secondName, departmentNo, salary);
+    }
+
+    public static Set<Emploee> getSetEmploees() {
+        Set<Emploee> emploees = new HashSet<>();
+        emploees.add(new Emploee("Ivanov", "Ivan", 1, 86811));
+        emploees.add(new Emploee("Petrov", "Petr", 2, 80000));
+        emploees.add(new Emploee("Ivanov", "Petr", 3, 99100));
+        emploees.add(new Emploee("Kumov", "Ivan", 4, 97600));
+        emploees.add(new Emploee("Gamov", "Egor", 5, 84667));
+        emploees.add(new Emploee("Sizov", "Oleg", 5, 73000));
+        emploees.add(new Emploee("Slikoff", "Mike", 4, 125400));
+        emploees.add(new Emploee("Krotov", "Andr", 3, 95410));
+        emploees.add(new Emploee("Vasin", "Nikolay", 1, 142000));
+        emploees.add(new Emploee("Gorin", "Nikk", 1, 82000));
+        return emploees;
     }
 }
